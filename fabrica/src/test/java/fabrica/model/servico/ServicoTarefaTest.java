d2e0281ad157467e07aa0f910f49b5507a14c36b
Eduardo Horst <eduardoquijano2@gmail.com>
2017-09-08 23:22:27 -0300

    docker build com dns

---
+++ fabrica/src/test/java/fabrica/model/servico/ServicoTarefaTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabrica.model.servico;
+
+import fabrica.dto.TarefaDTO;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author eddyosos
+ */
+public class ServicoTarefaTest {
+    
+    public ServicoTarefaTest() {
+    }
+    
+    ServicoTarefa servicoTarefa;
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void saniedade() {
+        assertTrue(true);
+    }
+    
+}
